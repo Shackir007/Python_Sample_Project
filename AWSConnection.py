510757babea58ca950ff1744b051f0463d83d10d
Shackir007 <Shackir007@yahoo.co.in>
2022-07-06 00:55:51 +0530

    Simply for testing

--- AWSConnection.py
+++ AWSConnection.py
@@ -1,5 +1,36 @@
+import secrets
 import boto3
 import pandas
+import json
 
+
+def readAccessKey():
+
+    pass
+
+
+def connectAWS():
+    access_key, secret_key = readAccessKey()
+    client = boto3.client('s3',
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_key,
+        region_name = 'ap-south-1'
+    )
+
+    # Creating the high level object oriented interface
+    resource = boto3.resource(
+        's3',
+        aws_access_key_id = access_key,
+        aws_secret_access_key = secret_key,
+        region_name='ap-south-1'
+    )
+
+    # Fetch the list of existing buckets
+    response = client.list_buckets()
+    bucket_list = response['Buckets']
+    print(bucket_list)
+
+
+connectAWS()
 
 
